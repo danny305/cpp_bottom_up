fcefad00625cfef69083de20ff5362dc834c9e24
Danny Diaz <danny.jesus.diaz.94@gmail.com>
2021-02-11 19:56:09 -0800

    Added docstrings to bindings.

--- bindings/shitTalker/shitTalker.cpp
+++ bindings/shitTalker/shitTalker.cpp
@@ -3,8 +3,12 @@
 namespace py = pybind11;
 
 PYBIND11_MODULE(cppShitTalker, m) {
-    py::class_<TalkShit>(m, "CppTalkShit")
+
+    m.doc() = "PyBind11 module for shitTalker!";
+
+    py::class_<TalkShit>(m, "CppTalkShit", "A class that talks shit and swallows spit.")
         .def(py::init())
-        .def("learnPhrase", &TalkShit::learnPhrase)
-        .def("saySomethingStupid", &TalkShit::saySomethingStupid);
+        .def("learnPhrase", &TalkShit::learnPhrase, "DocString for learning inappropriate phrases.")
+        .def("saySomethingStupid", &TalkShit::saySomethingStupid, 
+             "DocString for saySomethign Stupid");
 }
